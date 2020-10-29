76cbeae33eaa03ff5a4a0b48e77d48bd405337df
jito <jitokj3@gmail.com>
2020-10-29 12:41:03 +0530

    added video player

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Video from "./Video";
 
 function App() {
   return (
     <div className="app">
-      <h1>TikTok</h1>
+      <div className="app__videos">
+      <Video />
+      <Video />
+      </div>
+    
     </div>
   );
 }
--- src/Video.js
+++ src/Video.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import "./Video.css";
+import vid from "./video/video.MP4";
 
 const Video = () => {
+    const [playing,setPlaying] = useState(false);
+    const videoRef = useRef(null);
+    const handleVideoPress = ()=>{
+        if(playing){
+            videoRef.current.pause();
+            setPlaying(false);
+        }
+        else {
+            videoRef.current.play();
+            setPlaying(true);
+        }
+    }
     return (
-        <div>
+        <div className="video">
+            <video ref={videoRef}
+            onClick={handleVideoPress}
+            className="video_player" loop
+               src={vid} alt="video"></video>
             
         </div>
     );
